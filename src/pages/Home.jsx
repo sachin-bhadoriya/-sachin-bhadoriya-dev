25ceb24995defa8c1279f987da14bef6ac874275
Sachin Bhadoriya <bhadoriyasachin33@gmail.com>
2026-06-28 09:42:22 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import './styles/home.scss'
 const Home = () => {
   const url = "https://api.cloudinary.com/v1_1/dm0rfcoxl/image/download?api_key=292658888953328&attachment=true&audit_context=eyJhY3Rvcl90eXBlIjoidXNlciIsImFjdG9yX2lkIjoiNDgwMzJmODgxODEwZDVmZTVmODUzMjQ1Y2JmZGQwODkiLCJ1c2VyX2V4dGVybmFsX2lkIjoiNzRlYTc5YmNmNmJiYWI5NjhjNzgxMjJiMjhlNTY0IiwidXNlcl9jdXN0b21faWQiOiJiaGFkb3JpeWFzYWNoaW4zM0BnbWFpbC5jb20iLCJjb21wb25lbnQiOiJjb25zb2xlIn0%3D&public_id=Sachin_Bhadoriya_Frontend_Developer_i3hyqj&signature=9d5d3bff0cc80af4b3e525aefd086b4b105e916a&source=ml&target_filename=Sachin_Bhadoriya_Frontend_Developer&timestamp=1779813691&type=upload"
 return (
-    <section data-aos="fade" className='p-home-m-c'>
+    <section data-aos="fade" className='p-home-m-c container'>
       <img data-aos="zoom-in" src="https://res.cloudinary.com/dm0rfcoxl/image/upload/v1779808589/sachin-illustration_ibc3od.png" alt="Illustration Image" fetchPriority='high' />
       <div className="p-home-d-c">
         <h6>Hello, I'm</h6>
